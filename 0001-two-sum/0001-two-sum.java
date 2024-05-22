27a9a667dad4cb99d831995d8521d12ccfd67b1e
Hrishabh Ayush <84783956+hrishabhayush@users.noreply.github.com>
2024-05-22 09:57:43 -0400

    Time: 4 ms (62.46%), Space: 44.5 MB (89.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,33 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] ansSet = new int[2];
+        // int[] ansSet = new int[2];
+        // for (int i = 0; i < nums.length; i++) {
+        //     for (int j = 0; j < i; j++) {
+        //         if (nums[i] + nums[j] == target) {
+        //             ansSet[0] = i;
+        //             ansSet[1] = j;
+        //         }
+        //     }
+        // }
+        // return ansSet;
+        // Time complexity: O(n) and Space complexity: O(1)
+        
+        // Need a solution with better time complexity
+        // An approach of Hashamps seems reasonable here, because we then just look
+        // for the corresponding number that sums up to the target. 
+        
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] + nums[j] == target) {
-                    ansSet[0] = i;
-                    ansSet[1] = j;
-                }
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] {
+                    i, map.get(complement)
+                };
             }
         }
-        return ansSet;
+        return null; // If no solution found
     }
 }
