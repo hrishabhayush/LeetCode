4e28e7665df9e8f73b0aab57dc9fcfcaa2828d8d
Hrishabh Ayush <84783956+hrishabhayush@users.noreply.github.com>
2024-05-23 02:57:08 -0400

    Time: 1 ms (100.00%), Space: 55.2 MB (59.53%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+//         int ans = 1;
+//         int zeroProd = 1;
+//         int[] list = new int[nums.length];
+//         boolean bool = true;
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] != 0) {
+//                 bool = false;
+//             }
+//         }
+//         if (bool == true) {
+//             for (int i = 0; i < nums.length; i++) {
+//                 list[i] = 0;
+//             }
+//         }
+//         for (int i = 0; i < nums.length; i++) {
+//             ans *= nums[i];
+//             if (nums[i] == 0 && i != nums.length - 1) {
+//                 i++;
+//             }
+//             if (nums[i] == 0 && i == nums.length -1) {
+//                 break;
+//             }
+//             zeroProd *= nums[i];
+//         }
+        
+        
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] != 0) {
+//             list[i] = ans/nums[i];
+//             } else {
+//                 list[i] = zeroProd;
+//             }
+//         }
+//         return list;
+        
+        int n = nums.length;
+        int[] result = new int[n];
+        
+        // Step 1: Calculate prefix products and store them in the result array.
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+        
+        // Step 2: Calculate suffix products and update the result array.
+        int suffixProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= suffixProduct;
+            suffixProduct *= nums[i];
+        }
+        
+        return result;
+    }
+}
