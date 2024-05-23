8d44f2cd8ede02641dc8c9b6ab3f77daba63712e
Hrishabh Ayush <84783956+hrishabhayush@users.noreply.github.com>
2024-05-23 03:02:30 -0400

    Update 0238-product-of-array-except-self.java

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -38,13 +38,13 @@ class Solution {
         int n = nums.length;
         int[] result = new int[n];
         
-        // Step 1: Calculate prefix products and store them in the result array.
+        // Calculate prefix products and store them in the result array.
         result[0] = 1;
         for (int i = 1; i < n; i++) {
             result[i] = result[i - 1] * nums[i - 1];
         }
         
-        // Step 2: Calculate suffix products and update the result array.
+        // Calculate suffix products and update the result array.
         int suffixProduct = 1;
         for (int i = n - 1; i >= 0; i--) {
             result[i] *= suffixProduct;
