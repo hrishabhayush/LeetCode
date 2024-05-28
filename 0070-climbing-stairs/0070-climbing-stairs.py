b86b87b20094d81ee10e18d36a759911953a6d2c
Hrishabh Ayush <84783956+hrishabhayush@users.noreply.github.com>
2024-05-28 13:52:48 -0400

    Time: 16 ms (32.44%), Space: 11.5 MB (94.06%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        memo = {}
+        for i in range(1, n + 1):
+            if (i <= 2):
+                result = i 
+            else:
+                result = memo[i-1] + memo[i-2]   
+            memo[i] = result  
+        return memo[n]
